4bce46f477264923a5042078b8700394b4851be0
mariapan0330 <maria.pan0330@gmail.com>
2022-08-28 17:07:31 -0500

    added separate nav and advanced search forms

--- js/main.js
+++ js/main.js
@@ -9,10 +9,31 @@ console.log(apiKey);
 
 {
     // which form are we submitting?
-    navForm = document.getElementById('navForm')
+    let navForm = document.getElementById('navForm')
+    let advancedForm = document.getElementById('advancedForm')
 
+    // handle submit from any form
     async function handleNavSubmit(e){
-        console.log('NAV FORM Handling Submission...');
+        console.log('Handling NAV Submission...');
+        e.preventDefault()
+
+        let cityInput = e.target.navCityInput.value;
+        if (cityInput){
+            console.log(`SUBMITTED: city=${cityInput}`);
+            
+            let weatherData = await getWeatherData(cityInput)
+            cityInput.value = ''
+    
+            console.log('WEATHER INFO FROM NAV: ', weatherData);
+            buildInfoCards(weatherData)
+        } else {
+            console.warn('Nav search is empty');
+        }
+    }
+
+
+    async function handleAdvSubmit(e){
+        console.log('Handling ADVANCED Submission...');
         e.preventDefault()
 
         let cityInput = e.target.navCityInput.value;
@@ -21,43 +42,59 @@ console.log(apiKey);
         let latInput = e.target.latInput.value;
         let lonInput = e.target.lonInput.value;
 
+        console.log(`SUBMITTED: city=${cityInput} country=${countryInput} zip=${zipInput} lat=${latInput} lon=${lonInput}`);
         // if city alone, that's okay
         // if zip but no country, ERROR
         // if country but neither city nor zip, ERROR
         // if lat but no lon, ERROR
         // if lon but no lat, ERROR
-
-        if (zipInput !== '' && countryInput === '') {
+        if (zipInput && !countryInput) {
             console.warn('YES zip; NO country');
-        } else if (countryInput !== '' && (zipInput === '' &&)) {
+            buildErrorMessage();
+
+        } else if (countryInput && (!zipInput && !cityInput)) {
+            console.warn('YES country; NEITHER zip nor city');
+            buildErrorMessage();
+
+        } else if ((latInput && !lonInput) || (lonInput && !latInput)) {
+            console.warn('YES lat, NO lon; YES lon, NO lat');
+            buildErrorMessage();
+        } else {
+            let weatherInfo = await getWeatherData(cityInput, countryInput, zipInput, latInput, lonInput)
+            cityInput.value = ''
+            countryInput.value = ''
+            zipInput.value = ''
+            latInput.value = ''
+            lonInput.value = ''
+
+            console.log('WEATHER INFO FROM ADV SUBMIT: ',weatherInfo);
+            buildInfoCards(weatherData)
 
         }
-        
-        let weatherInfo = await getWeatherData(cityInput, countryInput, zipInput, latInput, lonInput)
-        cityInput.value = ''
-
-        
     }
 
     
     async function getWeatherData(city, country, zip, lat, lon){
         console.log('Getting Weather Data...');
 
-
-        
+        return 123
     }
 
 
     function buildInfoCards(weatherData){
-
+        console.log('Building Info Cards...');
     }
 
 
     function buildErrorMessage(...missing){
-
+        console.log('Building Error Message...');
     }
 
 
-    navForm.addEventListener('submit', handleNavSubmit)
-    advancedForm.addEventListener('submit', handleNavSubmit)
+    if (navForm){
+        navForm.addEventListener('submit', handleNavSubmit)
+    }
+    if (advancedForm){
+        advancedForm.addEventListener('submit', handleAdvSubmit)
+    }
 }
